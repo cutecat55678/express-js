5d7824dc09db1be3e7897fe68380bd58a16e6e6b
cutecat55678 <cutecat55678@outlook.com>
2021-10-28 01:11:06 +0530

    create sample endpoints for GET and POST requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET request example */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  try {
+    let user_id = req.query.user_id;
+    res.send(`Hello there ${user_id}`);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+/* POST request example */
+router.post('/', function(req, res, next) {
+  try {
+    let user_id = req.body.user_id;
+    let pwd = req.body.pwd;
+    if (user_id === 'ADMIN' && pwd === 'password') {
+      res.send('Your user_id and password are valid, welcome');
+    } else {
+      res.send('Sorry you are unauthorized');
+    }
+  } catch (e) {
+    return next(e);
+  }
 });
 
 module.exports = router;
